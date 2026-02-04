d4bb18d8f802e84df746b7b387da53c8caeb8482
dimakis <dimitri.saridakis@gmail.com>
2026-02-04 21:04:26 +0000

    feat(api): register OpenAI-compatible router in API

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -12,6 +12,7 @@ from src.api.routes.devices import router as devices_router
 from src.api.routes.entities import router as entities_router
 from src.api.routes.ha_registry import router as ha_registry_router
 from src.api.routes.insights import router as insights_router
+from src.api.routes.openai_compat import router as openai_router
 from src.api.routes.proposals import router as proposals_router
 from src.api.routes.system import router as system_router
 
@@ -29,5 +30,7 @@ api_router.include_router(chat_router)
 api_router.include_router(proposals_router)
 # User Story 3: Insights and Analysis
 api_router.include_router(insights_router)
+# OpenAI-compatible API for Open WebUI
+api_router.include_router(openai_router)
 
 __all__ = ["api_router"]
