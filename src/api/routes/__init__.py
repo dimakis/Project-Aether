621020ca2a1911da3b6920312cfbbe221cfff3c4
dimakis <dimitri.saridakis@gmail.com>
2026-02-04 14:59:14 +0000

    feat(api): register insights router in API

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -11,6 +11,7 @@ from src.api.routes.chat import router as chat_router
 from src.api.routes.devices import router as devices_router
 from src.api.routes.entities import router as entities_router
 from src.api.routes.ha_registry import router as ha_registry_router
+from src.api.routes.insights import router as insights_router
 from src.api.routes.proposals import router as proposals_router
 from src.api.routes.system import router as system_router
 
@@ -26,8 +27,7 @@ api_router.include_router(ha_registry_router, prefix="/registry")
 # User Story 2: Conversations and Proposals
 api_router.include_router(chat_router)
 api_router.include_router(proposals_router)
-
-# Future routers:
-# api_router.include_router(insights_router, prefix="/insights", tags=["Insights"])
+# User Story 3: Insights and Analysis
+api_router.include_router(insights_router)
 
 __all__ = ["api_router"]
